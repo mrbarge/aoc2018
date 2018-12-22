f19008ae9bea86c28eaa61e9645e295993477b14
Matt Bargenquast <matt.bargenquast@gmail.com>
2018-12-22 19:27:33 +1000

    day 22 solution

--- golang/day15/day15-g-alt.go
+++ golang/day15/day15-g-alt.go
@@ -6,7 +6,7 @@ import (
 	"os"
 	"regexp"
 	"sort"
-	"github.com/albertorestifo/dijkstra"
+	"github.com/RyanCarrier/dijkstra"
 	"strconv"
 )
 
@@ -30,7 +30,9 @@ const (
 	ELF = 'E'
 )
 
-func process(datafile string) ([][]Element, []*Actor, dijkstra.Graph) {
+var vertexMap = make(map[string]int, 0)
+
+func process(datafile string) ([][]Element, []*Actor) {
 
 	file, _ := os.Open(datafile)
 	s := bufio.NewScanner(file)
@@ -41,6 +43,7 @@ func process(datafile string) ([][]Element, []*Actor, dijkstra.Graph) {
 	actors := make([]*Actor, 0)
 	y := 0
 	actorId := 0
+	nodeId := 0
 	for s.Scan() {
 		line := s.Text()
 		mapline := make([]Element,len(line))
@@ -51,10 +54,14 @@ func process(datafile string) ([][]Element, []*Actor, dijkstra.Graph) {
 			case '#':
 				mapline[x] = WALL
 			case '.':
-				g[scoord] = make(map[string]int,0)
+				vertexMap[scoord] = nodeId
+				nodeId += 1
+				g.AddVertex(nodeId)
 				mapline[x] = SPACE
 			case 'G':
-				g[scoord] = make(map[string]int,0)
+				vertexMap[scoord] = nodeId
+				nodeId += 1
+				g.AddVertex(nodeId)
 				actor := new(Actor)
 				actor.id = actorId
 				actorId += 1
@@ -65,7 +72,9 @@ func process(datafile string) ([][]Element, []*Actor, dijkstra.Graph) {
 				mapline[x] = GOBLIN
 				actors = append(actors, actor)
 			case 'E':
-				g[scoord] = make(map[string]int,0)
+				vertexMap[scoord] = nodeId
+				nodeId += 1
+				g.AddVertex(nodeId)
 				actor := new(Actor)
 				actor.id = actorId
 				actorId += 1
@@ -88,15 +97,52 @@ func process(datafile string) ([][]Element, []*Actor, dijkstra.Graph) {
 				sc := fmt.Sprintf("%d,%d",x,y)
 				neighbours := getNeighboursForNode(x,y,gmap)
 				for _, n := range neighbours {
-					g[sc][n] = 1
-					g[n][sc] = 1
+					g.AddArc(vertexMap[sc],vertexMap[n],1)
+					g.AddArc(vertexMap[n],vertexMap[sc],1)
 				}
 			}
 		}
 	}
 	fmt.Println("Got here?")
 	printMap(gmap,actors)
-	return gmap, actors, g
+	return gmap, actors
+}
+
+func buildGraph(gmap [][]Element, source *Actor, actors []*Actor) dijkstra.Graph {
+
+	g := dijkstra.Graph{}
+	nodeId := 0
+	for y, _ := range gmap {
+		for x, _ := range gmap {
+			if gmap[y][x] != WALL {
+				sc := fmt.Sprintf("%d,%d", x, y)
+				vertexMap[sc] = nodeId
+				g.AddVertex(nodeId)
+				nodeId += 1
+			}
+		}
+	}
+	for y, _ := range gmap {
+		for x, _ := range gmap {
+			if gmap[y][x] != WALL {
+				sc := fmt.Sprintf("%d,%d", x, y)
+				neighbours := getNeighboursForNode(x,y,gmap)
+				if gmap[y][x] == SPACE  {
+					for _, n := range neighbours {
+						g.AddArc(vertexMap[sc], vertexMap[n], 1)
+						g.AddArc(vertexMap[n], vertexMap[sc], 1)
+					}
+				} else {
+					// preventative method of traversing this node
+					for _, n := range neighbours {
+						g.AddArc(vertexMap[sc], vertexMap[n], 1000)
+						g.AddArc(vertexMap[n], vertexMap[sc], 1000)
+					}
+				}
+			}
+		}
+	}
+	return g
 }
 
 func getNeighboursForNode(x int, y int, gmap [][]Element) []string {
@@ -111,6 +157,23 @@ func getNeighboursForNode(x int, y int, gmap [][]Element) []string {
 		coords = append(coords, fmt.Sprintf("%d,%d",x,y-1))
 	}
 	if gmap[y+1][x] != WALL {
+		coords = append(coords, fmt.Sprintf("%d,%d",x,y+1))
+	}
+	return coords
+}
+
+func getTypedNeighboursForNode(x int, y int, gmap [][]Element, t Element) []string {
+	coords := make([]string, 0)
+	if gmap[y][x-1] == t {
+		coords = append(coords, fmt.Sprintf("%d,%d",x-1,y))
+	}
+	if gmap[y][x+1] == t {
+		coords = append(coords, fmt.Sprintf("%d,%d",x+1,y))
+	}
+	if gmap[y-1][x] == t {
+		coords = append(coords, fmt.Sprintf("%d,%d",x,y-1))
+	}
+	if gmap[y+1][x] == t {
 		coords = append(coords, fmt.Sprintf("%d,%d",x,y+1))
 	}
 	return coords
@@ -133,6 +196,19 @@ func sortActorsByHp(actors []*Actor) []*Actor{
 		return actors[i].hp < actors[j].hp
 	})
 	return actors
+}
+
+func sortCoordsByLoc(coords []Coord) []Coord{
+	sort.Slice(coords, func(i, j int) bool {
+		if coords[i].y < coords[j].y {
+			return true
+		}
+		if coords[i].y > coords[j].y {
+			return false
+		}
+		return coords[i].x < coords[j].x
+	})
+	return coords
 }
 
 func sortActorsByLoc(actors []*Actor) []*Actor{
@@ -206,25 +282,30 @@ func attack(attacker *Actor, target *Actor) bool {
 	return target.hp <= 0
 }
 
-func removeActor(actor *Actor, actors []*Actor) []*Actor {
+func removeDeadActors(actors []*Actor) []*Actor {
+	retlist := make([]*Actor,0)
 	for pos := 0; pos < len(actors); pos++ {
-		if actors[pos].id == actor.id {
-			actors = append(actors[:pos], actors[pos+1:]...)
-			break
+		if actors[pos].hp > 0 {
+			retlist = append(retlist,actors[pos])
 		}
 	}
-	return actors
+	return retlist
 }
 
-func runGame(gmap [][]Element, actors []*Actor, graph dijkstra.Graph) {
+func runGame(gmap [][]Element, actors []*Actor) {
 
 	done := false
-	counter := 0
-	maxCount := 23
-	for !done && counter < maxCount {
+	counter := 1
+	maxCount := 24
+	for !done && counter <= maxCount {
 		// Get active actors in reading list order
 		actors = sortActorsByLoc(actors)
 		for _, actor := range actors {
+			// skip the dead ones
+			if actor.hp <= 0 {
+				continue
+			}
+
 			fmt.Printf("Handling actor (%d,%d)\n",actor.c.x,actor.c.y)
 			// Get nearest neighbouring targets
 			target := getNearestNeighbouringTarget(actor, actors)
@@ -237,14 +318,14 @@ func runGame(gmap [][]Element, actors []*Actor, graph dijkstra.Graph) {
 				// from the list of active actors
 				if isDead {
 					fmt.Printf("Target at (%d,%d) died\n",target.c.x,target.c.y)
-					actors = removeActor(target, actors)
+					//actors = removeActor(target, actors)
 					// clear the map
 					gmap[target.c.y][target.c.x] = SPACE
 				}
 			} else {
 				// Otherwise find nearest target to move to
 
-				nextCoord := move(actor,actors,gmap,graph)
+				nextCoord := move(actor,actors,gmap)
 				if nextCoord.x == actor.c.x && nextCoord.y == actor.c.y {
 					fmt.Printf("Actor at (%d,%d) doing nothing\n",actor.c.x,actor.c.y)
 					continue
@@ -264,7 +345,6 @@ func runGame(gmap [][]Element, actors []*Actor, graph dijkstra.Graph) {
 					isDead := attack(actor, target)
 					if isDead {
 						fmt.Printf("Target at (%d,%d) died\n", target.c.x, target.c.y)
-						actors = removeActor(target, actors)
 						// clear the map
 						gmap[target.c.y][target.c.x] = SPACE
 					}
@@ -275,35 +355,33 @@ func runGame(gmap [][]Element, actors []*Actor, graph dijkstra.Graph) {
 			if !remainingTargets(actors, actor.isGoblin) {
 				fmt.Println("No more targets")
 				done = true
+				break
 			}
 		}
-		counter += 1
+		// remove the dead ones
+		actors = removeDeadActors(actors)
+		if !done {
+			counter += 1
+		}
 		printMap(gmap,actors)
 	}
+	fmt.Printf("Output: %d %d - %d\n",sumOutput(actors),counter,sumOutput(actors)*counter)
+
 }
 
-func move(source *Actor, actors []*Actor, gmap [][]Element, graph dijkstra.Graph) Coord {
-
-	actors = sortActorsByLoc(actors)
-
-	sourcestr := fmt.Sprintf("%d,%d",source.c.x,source.c.y)
-	// set the transition arcs out of occupied nodes to be stupidly huge so they're ignored
-	for _, actor := range actors {
-		ac := fmt.Sprintf("%d,%d",actor.c.x,actor.c.y)
-		if actor.id == source.id {
-			// don't do it for our source
-			continue
-		} else {
-			for k, _ := range graph[ac] {
-				graph[ac][k] = 1000
-			}
+func sumOutput(actors []*Actor) int {
+	ret := 0
+	for _, a := range actors {
+		if a.hp > 0 {
+			ret += a.hp
 		}
 	}
+	return ret
+}
 
-	// build up a list of preferred targets in order of reading, then distance
-	preferredTargets := make([]*Actor,0)
-	minCost := 0
-	minPath := make([]string, 0)
+func getMoveCandidates(source *Actor, actors []*Actor, gmap [][]Element) []Coord {
+
+	retc := make([]Coord,0)
 	for _, actor := range actors {
 		if actor.id == source.id {
 			// don't do it for our source
@@ -312,10 +390,48 @@ func move(source *Actor, actors []*Actor, gmap [][]Element, graph dijkstra.Graph
 			// don't kill our own kind
 			continue
 		}
-		ac := fmt.Sprintf("%d,%d",actor.c.x,actor.c.y)
-		fmt.Printf("Finding path to %s\n", ac)
-		path, cost, err := graph.Path(sourcestr,ac)
+		c := getTypedNeighboursForNode(actor.c.x, actor.c.y,gmap,SPACE)
+		for _, v := range c {
+			cv := keyToCoord(v)
+			add := true
+			for _, v2 := range retc {
+				if v2.x == cv.x && v2.y == cv.y {
+					add = false
+					break
+				}
+			}
+			if add {
+				retc = append(retc,cv)
+			}
+		}
+	}
 
+	// sort in reading order
+	retc = sortCoordsByLoc(retc)
+	return retc
+}
+
+func move(source *Actor, actors []*Actor, gmap [][]Element) Coord {
+
+	sourcestr := fmt.Sprintf("%d,%d",source.c.x,source.c.y)
+
+	// set the transition arcs out of occupied nodes to be stupidly huge so they're ignored
+	graph := buildGraph(gmap,source,actors)
+
+	// get a list of coordinates as shortest path destinations
+	moveCandidates := getMoveCandidates(source, actors, gmap)
+	fmt.Print("Move candidates are: ")
+	fmt.Println(moveCandidates)
+
+	// build up a list of preferred coords in order of reading, then distance
+	preferredTargets := make([]Coord,0)
+	minCost := 0
+	minPath := make([]int,0)
+	for _, moveCandidate := range moveCandidates {
+		ac := fmt.Sprintf("%d,%d",moveCandidate.x,moveCandidate.y)
+		fmt.Printf("Finding path to %s\n", ac)
+		best, err := graph.Shortest(vertexMap[sourcestr],vertexMap[ac])
+		fmt.Println(best.Path)
 
 		if err != nil {
 			fmt.Print("ERror occurred: ")
@@ -323,35 +439,32 @@ func move(source *Actor, actors []*Actor, gmap [][]Element, graph dijkstra.Graph
 			return source.c
 		} else {
 			if len(preferredTargets) == 0 {
-				minCost = cost
-				preferredTargets = append(preferredTargets, actor)
-				minPath = path
-			} else if cost < minCost {
-				preferredTargets = append([]*Actor{actor}, preferredTargets...)
-				minCost = cost
-				minPath = path
+				minCost = int(best.Distance)
+				preferredTargets = append(preferredTargets, moveCandidate)
+				minPath = best.Path
+			} else if int(best.Distance) <= minCost {
+				preferredTargets = append([]Coord{moveCandidate}, preferredTargets...)
+				minCost = int(best.Distance)
+				minPath = best.Path
 			} else {
-				preferredTargets = append(preferredTargets, actor)
+				preferredTargets = append(preferredTargets, moveCandidate)
 			}
 		}
 	}
 
-	// reset the graph?
-	for _, actor := range actors {
-		ac := fmt.Sprintf("%d,%d",actor.c.x,actor.c.y)
-		if actor.id == source.id {
-			// don't do it for our source
-			continue
-		} else {
-			for k, _ := range graph[ac] {
-				graph[ac][k] = 1
+	if len(minPath) == 0 {
+		return source.c
+	} else {
+		for k, v := range vertexMap {
+			if v == minPath[1] {
+				retc := keyToCoord(k)
+				fmt.Printf("Actor at %s moving to closest enemy (%s)\n", sourcestr, minPath[1])
+				return retc
 			}
 		}
+		fmt.Println("Hopefully should not happen")
+		return source.c
 	}
-
-	retc := keyToCoord(minPath[1])
-	fmt.Printf("Actor at %s moving to closest enemy (%s)\n",sourcestr,minPath[1])
-	return retc
 }
 
 func keyToCoord(k string) Coord {
@@ -395,7 +508,7 @@ func printMap(gmap [][]Element, actors []*Actor) {
 
 func main() {
 	//	process("input.dat")
-	gmap, actors, graph := process("test.dat")
-	runGame(gmap, actors, graph)
+	gmap, actors := process("test.dat")
+	runGame(gmap, actors)
 }
 
