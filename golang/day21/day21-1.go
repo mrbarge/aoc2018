c596b3ee3516510306f25a117ef978f13f05677d
Matt Bargenquast <matt.bargenquast@gmail.com>
2018-12-22 19:29:49 +1000

    day 22-1 and day21-1

--- golang/day21/day21-1.go
+++ golang/day21/day21-1.go
@@ -2,28 +2,160 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"regexp"
+	"strconv"
 )
 
-func process(datafile string) {
+type Register int
+const (
+	REG_A = 1
+	REG_B = 2
+	REG_C = 3
+)
+
+type Operation int
+const (
+	BANI = iota		// LOCKED 0
+	GTRI			// LOCKED 1
+	SETI			// LOCKED 2
+	EQIR			// LOCKED 3
+	EQRR			// LOCKED 4
+	BORR			// LOCKED 5
+	BORI			// LOCKED 6
+	BANR			// LOCKED 7
+	MULI			// LOCKED 8
+	EQRI			// LOCKED 9
+	MULR			// LOCKED 10
+	GTRR			// LOCKED 11
+	SETR			// LOCKED 12
+	ADDR			// LOCKED 13
+	GTIR			// LOCKED 14
+	ADDI			// LOCKED 15
+)
+
+var operationMap = map[string]int{"bani":0,"gtri":1,"seti":2,"eqir":3,"eqrr":4,
+	"borr":5,"bori":6,"banr":7,"muli":8,"eqri":9,"mulr":10,"gtrr":11,"setr":12,"addr":13,"gtir":14,"addi":15}
+
+func process(datafile string, ip int) {
 
 	file, _ := os.Open(datafile)
 	s := bufio.NewScanner(file)
 
-	r_stub, _ := regexp.Compile(`^stub$`)
+	r_reg_op, _ := regexp.Compile(`^(\w+)\s+(\d+)\s+(\d+)\s+(\d+)$`)
 
+	instructions := make([][]int, 0)
 	for s.Scan() {
 		line := s.Text()
-		res_stub := r_stub.FindStringSubmatch(line)
-
+		res_reg_op := r_reg_op.FindStringSubmatch(line)
+		if res_reg_op != nil {
+			op_data := make([]int, 4)
+			op_data[0], _ = operationMap[res_reg_op[1]]
+			op_data[1], _ = strconv.Atoi(res_reg_op[2])
+			op_data[2], _ = strconv.Atoi(res_reg_op[3])
+			op_data[3], _ = strconv.Atoi(res_reg_op[4])
+			instructions = append(instructions, op_data)
+		}
 	}
 
+	for counter := 0; counter < 1; counter++ {
+		reg := []int{counter,0,0,0,0,0}
+		nextInstruction := 0
+
+		done := false
+		for !done {
+			op_data := instructions[nextInstruction]
+			reg,nextInstruction = operate(reg,op_data,Operation(op_data[0]), ip, nextInstruction)
+			if nextInstruction  < 0 || nextInstruction  >= len(instructions) {
+				fmt.Println("Exited for counter %d\n",counter)
+				done = true
+			}
+		}
+		if counter % 100 == 0 {
+			fmt.Printf("Counter %d\n",counter)
+		}
+	}
 }
 
+func operate(input []int, op_value []int, op Operation, ip int, ni int) ([]int,int) {
+	retreg := make([]int, len(input))
+	for i, _ := range input {
+		retreg[i] = input[i]
+	}
+
+	// write the instruction pointer to the register it is bound to
+	retreg[ip] = ni
+
+//	fmt.Printf("Before operation: %v\n", retreg)
+
+	switch op {
+	case ADDR:
+		retreg[op_value[REG_C]] = retreg[op_value[REG_A]] + retreg[op_value[REG_B]]
+	case ADDI:
+		retreg[op_value[REG_C]] = retreg[op_value[REG_A]] + op_value[REG_B]
+	case MULR:
+		retreg[op_value[REG_C]] = retreg[op_value[REG_A]] * retreg[op_value[REG_B]]
+	case MULI:
+		retreg[op_value[REG_C]] = retreg[op_value[REG_A]] * op_value[REG_B]
+	case BANR:
+		retreg[op_value[REG_C]] = retreg[op_value[REG_A]] & retreg[op_value[REG_B]]
+	case BANI:
+		retreg[op_value[REG_C]] = retreg[op_value[REG_A]] & op_value[REG_B]
+	case BORR:
+		retreg[op_value[REG_C]] = retreg[op_value[REG_A]] | retreg[op_value[REG_B]]
+	case BORI:
+		retreg[op_value[REG_C]] = retreg[op_value[REG_A]] | op_value[REG_B]
+	case SETR:
+		retreg[op_value[REG_C]] = retreg[op_value[REG_A]]
+	case SETI:
+		retreg[op_value[REG_C]] = op_value[REG_A]
+	case GTIR:
+		if op_value[REG_A] > retreg[op_value[REG_B]] {
+			retreg[op_value[REG_C]] = 1
+		} else {
+			retreg[op_value[REG_C]] = 0
+		}
+	case GTRI:
+		if retreg[op_value[REG_A]] > op_value[REG_B] {
+			retreg[op_value[REG_C]] = 1
+		} else {
+			retreg[op_value[REG_C]] = 0
+		}
+	case GTRR:
+		if retreg[op_value[REG_A]] > retreg[op_value[REG_B]] {
+			retreg[op_value[REG_C]] = 1
+		} else {
+			retreg[op_value[REG_C]] = 0
+		}
+	case EQIR:
+		if op_value[REG_A] == retreg[op_value[REG_B]] {
+			retreg[op_value[REG_C]] = 1
+		} else {
+			retreg[op_value[REG_C]] = 0
+		}
+	case EQRI:
+		if retreg[op_value[REG_A]] == op_value[REG_B] {
+			retreg[op_value[REG_C]] = 1
+		} else {
+			retreg[op_value[REG_C]] = 0
+		}
+	case EQRR:
+		if retreg[op_value[REG_A]] == retreg[op_value[REG_B]] {
+			retreg[op_value[REG_C]] = 1
+		} else {
+			retreg[op_value[REG_C]] = 0
+		}
+	}
+
+	if ni == 28 {
+		fmt.Printf("After operation: %v (%d)\n", retreg, ni)
+	}
+	nextInstruction := retreg[ip]
+	return retreg, nextInstruction+1
+}
 
 func main() {
-//	process("input.dat")
-	process("test.dat")
+	process("input.dat",5)
 }
 
--- golang/day22/day22-1.go
+++ golang/day22/day22-1.go
@@ -1,29 +1,149 @@
 package main
 
 import (
-	"bufio"
-	"os"
-	"regexp"
+	"fmt"
 )
 
-func process(datafile string) {
+type Region int
+const (
+	ROCKY = iota
+	WET
+	NARROW
+)
 
-	file, _ := os.Open(datafile)
-	s := bufio.NewScanner(file)
+type Tool int
+const (
+	CLIMBING = iota
+	TORCH
+	NEITHER
+)
 
-	r_stub, _ := regexp.Compile(`^stub$`)
+type Element struct {
+	region Region
+	c Coord
+	geologic int
+	erosion int
+}
 
-	for s.Scan() {
-		line := s.Text()
-		res_stub := r_stub.FindStringSubmatch(line)
+type Coord struct {
+	x int
+	y int
+}
 
+func process(depth int, targetX int, targetY int) {
+
+	maxX := targetX+1
+	maxY := targetY+1
+
+	grid := make([][]*Element, maxY)
+
+	// fill out zero range first
+	for iy := 0; iy <= targetY; iy++ {
+		r := make([]*Element, maxX)
+		grid[iy] = r
+		for ix := 0; ix <= targetX; ix++ {
+			e := new(Element)
+			grid[iy][ix] = e
+			e.c = Coord{ix,iy}
+			e.geologic = geologic(e.c, grid)
+			e.erosion = erosion(e.c, depth, grid)
+			e.region = region(e.c, grid)
+
+		}
+	}
+
+	// and the rest
+	for iy := 1; iy <= targetY; iy++ {
+		for ix := 1; ix <= targetX; ix++ {
+			e := new(Element)
+			grid[iy][ix] = e
+			e.c = Coord{ix,iy}
+			e.geologic = geologic(e.c, grid)
+			e.erosion = erosion(e.c, depth, grid)
+			e.region = region(e.c, grid)
+		}
+	}
+
+	fmt.Println("Part 1: %d", risk(grid,targetX,targetY))
+	printGrid(grid,targetX,targetY)
+}
+
+func printGrid(grid [][]*Element, targetX int, targetY int) {
+	for iy := 0; iy <= targetY; iy++ {
+		for ix := 0; ix <= targetX; ix++ {
+			if (ix == 0 && iy == 0) {
+				fmt.Print("M")
+			} else {
+				if grid[iy][ix].region == WET {
+					fmt.Print("=")
+				} else if grid[iy][ix].region == NARROW {
+					fmt.Print("|")
+				} else if grid[iy][ix].region == ROCKY {
+					fmt.Print(".")
+				}
+			}
+		}
+		fmt.Println()
 	}
 
 }
 
+func risk(grid [][]*Element, targetX int, targetY int) int {
+
+	retcnt := 0
+	for iy := 0; iy <= targetY; iy++ {
+		for ix := 0; ix <= targetX; ix++ {
+			if (ix == 0 && iy == 0) {
+				continue
+			}
+			if (ix == targetX && iy == targetY) {
+				continue
+			}
+			if grid[iy][ix].region == WET {
+				retcnt += 1
+			} else if grid[iy][ix].region == NARROW {
+				retcnt += 2
+			}
+		}
+	}
+	return retcnt
+}
+
+func geologic(c Coord, grid [][]*Element) int {
+	if c.x == 0 && c.y == 0 {
+		return 0
+	}
+
+	if c.x == 14 && c.y == 796 {
+		return 0
+	}
+
+	if c.y == 0 {
+		return c.x * 16807
+	}
+
+	if c.x == 0 {
+		return c.y * 48271
+	}
+
+	return grid[c.y-1][c.x].erosion * grid[c.y][c.x-1].erosion
+}
+
+func erosion(c Coord, depth int, grid [][]*Element) int {
+	return (grid[c.y][c.x].geologic + depth) % 20183
+}
+
+func region(c Coord, grid [][]*Element) Region {
+	if grid[c.y][c.x].erosion % 3 == 0 {
+		return ROCKY
+	} else if grid[c.y][c.x].erosion % 3 == 1 {
+		return WET
+	} else {
+		return NARROW
+	}
+}
 
 func main() {
-//	process("input.dat")
-	process("test.dat")
+	process(5355, 14, 796)
 }
 
